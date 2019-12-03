0369ddb218cf8882bd73b3d8f2436ca00a8de421
coolbeatz71 <sigmacool@gmail.com>
2019-12-04 00:36:49 +0200

    chore(project-setup): configure the project boilerplate - Prepare the TS environment - setup the Mongoose and MongoDB environment - setup linting utilities - finalize setup and build the app [Delivers]

---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const router: Router = Router();
+
+// TODO: add routes to be used
+
+// router.use('/', auth);
+// router.use('/users', users);
+
+export default router;
